ffbefe5508992400ff1df64031ae2b4ceff73830
20211576 <k.gheytuli@student.tue.nl>
2024-06-13 18:50:05 +0200

    Scenario 20.2: Memory Leak

--- test_simple.py
+++ test_simple.py
@@ -5,7 +5,7 @@ from simple import my_function
 class TestMain(unittest.TestCase):
 
     def test_memory_leak(self):
-        big_list = generate_memory_leak()
+        big_list = my_function()
         self.assertIsNotNone(big_list)
 
 if __name__ == '__main__':
