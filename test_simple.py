f2a367bc8e89c306e4ccfafabf6794b4577445c4
20211576 <k.gheytuli@student.tue.nl>
2024-06-13 18:30:55 +0200

    Fixed Scenario 17.1: Unit test fail

--- test_simple.py
+++ test_simple.py
@@ -4,10 +4,10 @@ from simple import add, subtract
 class TestMain(unittest.TestCase):
 
     def test_add(self):
-        self.assertEqual(add(3, 4), 7)  # This should pass
+        self.assertEqual(add(3, 4), 7)
 
     def test_subtract(self):
-        self.assertEqual(subtract(10, 5), 3)  # This should fail, expected 5 but incorrect logic
+        self.assertEqual(subtract(10, 5), 5) 
 
 if __name__ == '__main__':
     unittest.main()
